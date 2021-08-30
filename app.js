4e8baf7f86d4840a3f15d8a513ce3cafed4835d0
Park Seng Chul <ochestra365@naver.com>
2021-08-30 11:55:29 +0900

    20210830_1155 ajax-form.html에서 오류발생

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.listen(3000,function(){
     console.log("start! express server on port 3000");
 });
 app.use(express.static('public'))//퍼블릭이란 디렉토리를 정적으로 기억하겠다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))//외우는 것이다. --> 클라이언트랑 서버랑 데이터를 주고 받을 때 인코딩 데이터를 이용한다. 다른 특수문자는 치환해서 보낸다. 그걸 인코딩이라 한다.
+app.set('view engine','ejs')//이 문자열을 찾아서 view engine을 사용한다.
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/public/main.html")
 })
@@ -15,5 +18,18 @@ app.get('/main',function(req,res){
 
 app.post('/email_post',function(req,res){
     //get : req.param('email')
-    res.send("post response")
+    console.log(req.body.email)
+    //res.send("<h1>welcome "+req.body.email+"</h1>")
+    res.render('email.ejs',{'email' : req.body.email})//이메일 키 값에 해당 value를 집어 넣어줘라. 그러면 치환을 해서 클라이언트에 응답을 해준다.
 })
+
+app.post('/ajax_send_email',function(req,res){
+    console.log(req.body.email);
+    var responseData={'result': 'OK','email':req.body.email};
+    res.json(responseData)
+})
+app.post('/ajax_send_email',function(req,res){
+    console.log(req.body.email);
+    var responseData={'result':'ok','email':req.body.email}
+    res.json(responseData)
+});
